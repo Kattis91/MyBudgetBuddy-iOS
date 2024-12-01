9582d60bf87ed3cf9f1941ee3be5343adc142570
Kattis91 <katya.svedmark@gmail.com>
2024-12-01 10:16:30 +0100

    Add Home and Login views. Add login/register logic

---
+++ MyBudget/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  MyBudget
+//
+//  Created by Katya Durneva Svedmark on 2024-12-01.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
