96e437fc1ef88b9b48091bba612464c04842c757
Kattis91 <katya.svedmark@gmail.com>
2024-12-01 10:16:48 +0100

    Add Home and Login views. Add login/register logic

--- MyBudget/ContentView.swift
+++ MyBudget/ContentView.swift
@@ -7,15 +7,34 @@
 
 import SwiftUI
 
+import FirebaseAuth
+
+
 struct ContentView: View {
+    
+    @State var isLoggedIn : Bool?
+    
     var body: some View {
+      
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if isLoggedIn == true {
+                HomeView()
+            }
+            if isLoggedIn == false {
+                LoginView()
+            }
         }
-        .padding()
+        .onAppear() {
+            Auth.auth().addStateDidChangeListener { auth, user in
+                print("USER CHANGE")
+                
+                if Auth.auth().currentUser == nil {
+                    isLoggedIn = false
+                } else {
+                    isLoggedIn = true
+                }
+            }
+        }
     }
 }
 
--- MyBudget/LoginView.swift
+++ MyBudget/LoginView.swift
@@ -6,10 +6,52 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseAuth
 
 struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("LOGIN")
+            TextField("Email", text: $email)
+            SecureField("Password", text: $password)
+            
+            Button(action: {
+                Task {
+                    await userLogin()
+                }
+            }) {
+                Text("Login")
+            }
+            Button(action: {
+                Task {
+                    await userRegister()
+                }
+            }) {
+                Text("Register")
+            }
+        }
+        .padding()
+    }
+    
+    func userLogin() async {
+        do {
+            try await Auth.auth().signIn(withEmail: email, password: password)
+        } catch {
+            print("Fel Login")
+        }
+    }
+    
+    func userRegister() async {
+        do {
+            let regResult = try await Auth.auth().createUser(withEmail: email, password: password)
+        } catch {
+            print("Fel Reg")
+        }
     }
 }
 
