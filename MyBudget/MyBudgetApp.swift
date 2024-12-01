bb99ac79a8ee1e9d4097b1b4ab74ed74264670ce
Kattis91 <katya.svedmark@gmail.com>
2024-12-01 09:54:26 +0100

    Initial Commit

---
+++ MyBudget/MyBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBudgetApp.swift
+//  MyBudget
+//
+//  Created by Katya Durneva Svedmark on 2024-12-01.
+//
+
+import SwiftUI
+
+@main
+struct MyBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
