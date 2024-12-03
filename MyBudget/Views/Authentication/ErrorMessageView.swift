0240663cbf124017f13c483499c7a5d5047650b5
Kattis91 <katya.svedmark@gmail.com>
2024-12-03 21:43:20 +0100

    Refactor the code by creating ErrorMessageView

---
+++ MyBudget/Views/Authentication/ErrorMessageView.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorMessageView.swift
+//  MyBudget
+//
+//  Created by Katya Durneva Svedmark on 2024-12-03.
+//
+
+import SwiftUI
+
+struct ErrorMessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ErrorMessageView()
+}
