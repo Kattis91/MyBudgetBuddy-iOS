c5bd47e83c15e632c1988c8224ead4529f6d3819
Kattis91 <katya.svedmark@gmail.com>
2025-02-11 17:45:29 +0100

    Add ScrollView to make it work on smaller screen sizes

--- MyBudget/Views/Authentication/RegisterView.swift
+++ MyBudget/Views/Authentication/RegisterView.swift
@@ -21,48 +21,50 @@ struct RegisterView: View {
     @State private var generalErrorMessage = ""
     
     var body: some View {
-        VStack {
-            
+        ScrollView {
             VStack {
                 
-                Image("Save")
-                    .resizable()
-                    .frame(width: 170, height: 170)
-                    .padding(.bottom, 70)
-                
-                CustomTextFieldView(placeholder: "Email", text: $email, onChange: {
-                    emailErrorMessage = ""
-                }, leadingPadding: 45, trailingPadding: 45, systemName: "envelope", maxLength: 50)
-                
-                ErrorMessageView(errorMessage: emailErrorMessage, height: 15, padding: 30)
-                
-                CustomTextFieldView(placeholder: "Password", text: $password, isSecure: true, onChange: {
-                    passwordErrorMessage = ""
-                }, leadingPadding: 45, trailingPadding: 45, systemName: "lock")
-                
-                ErrorMessageView(errorMessage: passwordErrorMessage, height: 15, padding: 30)
-                
-                CustomTextFieldView(placeholder: "Confirm Password", text: $confirmPassword, isSecure: true, onChange: {
-                    confirmPasswordErrorMessage = ""
-                }, leadingPadding: 45, trailingPadding: 45, systemName: "lock")
-                
-                ErrorMessageView(errorMessage: confirmPasswordErrorMessage, height: 15, padding: 30)
-            
-                ErrorMessageView(errorMessage: generalErrorMessage, padding: 30)
-                
-                Button(action: {
-                    emailErrorMessage = ValidationUtils.validateEmail(email: email) ?? ""
-                    passwordErrorMessage = ValidationUtils.validatePassword(password: password) ?? ""
-                    confirmPasswordErrorMessage = ValidationUtils.validateConfirmPassword(password: password, confirmPassword: confirmPassword) ?? ""
+                VStack {
                     
-                    if emailErrorMessage.isEmpty && passwordErrorMessage.isEmpty && confirmPasswordErrorMessage.isEmpty {
-                        budgetfb.userRegister(email: email, password: password) { firebaseError in
-                            generalErrorMessage = firebaseError ?? ""
+                    Image("Save")
+                        .resizable()
+                        .frame(width: 170, height: 170)
+                        .padding(.bottom, 70)
+                    
+                    CustomTextFieldView(placeholder: "Email", text: $email, onChange: {
+                        emailErrorMessage = ""
+                    }, leadingPadding: 45, trailingPadding: 45, systemName: "envelope", maxLength: 50)
+                    
+                    ErrorMessageView(errorMessage: emailErrorMessage, height: 15, padding: 30)
+                    
+                    CustomTextFieldView(placeholder: "Password", text: $password, isSecure: true, onChange: {
+                        passwordErrorMessage = ""
+                    }, leadingPadding: 45, trailingPadding: 45, systemName: "lock")
+                    
+                    ErrorMessageView(errorMessage: passwordErrorMessage, height: 15, padding: 30)
+                    
+                    CustomTextFieldView(placeholder: "Confirm Password", text: $confirmPassword, isSecure: true, onChange: {
+                        confirmPasswordErrorMessage = ""
+                    }, leadingPadding: 45, trailingPadding: 45, systemName: "lock")
+                    
+                    ErrorMessageView(errorMessage: confirmPasswordErrorMessage, height: 15, padding: 30)
+                    
+                    ErrorMessageView(errorMessage: generalErrorMessage, padding: 30)
+                    
+                    Button(action: {
+                        emailErrorMessage = ValidationUtils.validateEmail(email: email) ?? ""
+                        passwordErrorMessage = ValidationUtils.validatePassword(password: password) ?? ""
+                        confirmPasswordErrorMessage = ValidationUtils.validateConfirmPassword(password: password, confirmPassword: confirmPassword) ?? ""
+                        
+                        if emailErrorMessage.isEmpty && passwordErrorMessage.isEmpty && confirmPasswordErrorMessage.isEmpty {
+                            budgetfb.userRegister(email: email, password: password) { firebaseError in
+                                generalErrorMessage = firebaseError ?? ""
+                            }
                         }
+                    }) {
+                        ButtonView(buttontext: "Sign Up".uppercased(), maxWidth: 150, expenseButton: true)
+                            .shadow(color: .black.opacity(0.3), radius: 4, x: 0, y: 2)
                     }
-                }) {
-                    ButtonView(buttontext: "Sign Up".uppercased(), maxWidth: 150, expenseButton: true)
-                        .shadow(color: .black.opacity(0.3), radius: 4, x: 0, y: 2)
                 }
             }
         }
